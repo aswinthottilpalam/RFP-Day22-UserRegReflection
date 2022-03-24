7b6a8779f4b4c5550064a92bb0b031096e872cf5
aswinthottilpalam <aswinp264@gmail.com>
2022-03-24 10:27:43 +0530

    User registration using reflection

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Using Reflection!");
+        }
+    }
+}
